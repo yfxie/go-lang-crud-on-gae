5328b65a8371f9f2c093629a23f52423e9dccdb5
CatCfrco <z82206.cat@gmail.com>
2013-09-22 16:36:18 +0800

    retab

--- golang_gae_crud/main.go
+++ golang_gae_crud/main.go
@@ -1,70 +1,70 @@
 package golang_gae_crud
 
 import (
-	"appengine"
-	"appengine/datastore"
-	"appengine/user"
-	"fmt"
-	"html/template"
-	"net/http"
-	"strconv"
-	"time"
+    "appengine"
+    "appengine/datastore"
+    "appengine/user"
+    "fmt"
+    "html/template"
+    "net/http"
+    "strconv"
+    "time"
 )
 
 type Greeting struct {
-	Id      int64
-	Author  string
-	Content string
-	Date    time.Time
+    Id      int64
+    Author  string
+    Content string
+    Date    time.Time
 }
 
 type Context struct {
-	Greetings   []Greeting
-	CurrentUser string
+    Greetings   []Greeting
+    CurrentUser string
 }
 
 func init() {
-	router()
+    router()
 }
 
 func router() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/create", create)
-	http.HandleFunc("/destroy", destroy)
-	http.HandleFunc("/edit", edit)
-	http.HandleFunc("/update", update)
+    http.HandleFunc("/", index)
+    http.HandleFunc("/create", create)
+    http.HandleFunc("/destroy", destroy)
+    http.HandleFunc("/edit", edit)
+    http.HandleFunc("/update", update)
 }
 
 func guestbookKey(c appengine.Context) *datastore.Key {
-	return datastore.NewKey(c, "Guestbook", "default_guestbook", 0, nil)
+    return datastore.NewKey(c, "Guestbook", "default_guestbook", 0, nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	if u := user.Current(c); u == nil {
-		url, err := user.LoginURL(c, r.URL.String())
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Location", url)
-		w.WriteHeader(http.StatusFound)
-		return
-	}
-	q := datastore.NewQuery("Greeting").Ancestor(guestbookKey(c)).Order("-Date").Limit(10)
-	greetings := make([]Greeting, 0, 10)
-	keys, err := q.GetAll(c, &greetings)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	for i := 0; i < len(greetings); i++ {
-		greetings[i].Id = keys[i].IntID()
-	}
-	context := Context{Greetings: greetings, CurrentUser: user.Current(c).String()}
-	if err := guestbookTemplate.Execute(w, context); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+    c := appengine.NewContext(r)
+    if u := user.Current(c); u == nil {
+        url, err := user.LoginURL(c, r.URL.String())
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.Header().Set("Location", url)
+        w.WriteHeader(http.StatusFound)
+        return
+    }
+    q := datastore.NewQuery("Greeting").Ancestor(guestbookKey(c)).Order("-Date").Limit(10)
+    greetings := make([]Greeting, 0, 10)
+    keys, err := q.GetAll(c, &greetings)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    for i := 0; i < len(greetings); i++ {
+        greetings[i].Id = keys[i].IntID()
+    }
+    context := Context{Greetings: greetings, CurrentUser: user.Current(c).String()}
+    if err := guestbookTemplate.Execute(w, context); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
 
 }
 
@@ -77,18 +77,18 @@ const guestbookTemplateHTML = `
       <div><textarea name="content" rows="3" cols="60"></textarea></div>
       <div><input type="submit" value="create"></div>
     </form>
-  	{{ $CurrentUser := .CurrentUser}}
-  	<ul>
+    {{ $CurrentUser := .CurrentUser}}
+    <ul>
     {{range .Greetings}}
-    	<li>
-      	{{with .Author}}
-        	<b>{{.}}</b> said:
-      	{{else}}
-        	Guest said:
-      	{{end}}
-      	<pre>{{.Content}}</pre>
-      	<p>{{ if eq $CurrentUser .Author}} <a href="/edit?id={{.Id}}">Edit</a> / <a href="/destroy?id={{.Id}}">Delete</a> {{end}}</p>
-    	</li>
+    <li>
+    {{with .Author}}
+        <b>{{.}}</b> said:
+    {{else}}
+        Guest said:
+    {{end}}
+    <pre>{{.Content}}</pre>
+    <p>{{ if eq $CurrentUser .Author}} <a href="/edit?id={{.Id}}">Edit</a> / <a href="/destroy?id={{.Id}}">Delete</a> {{end}}</p>
+    </li>
     {{end}}
     </ul>
 
@@ -97,51 +97,51 @@ const guestbookTemplateHTML = `
 `
 
 func create(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	u := user.Current(c)
-	if u == nil {
-		fmt.Fprint(w, "permission denied")
-		return
-	}
-	g := Greeting{
-		Author:  u.String(),
-		Content: r.FormValue("content"),
-		Date:    time.Now(),
-	}
+    c := appengine.NewContext(r)
+    u := user.Current(c)
+    if u == nil {
+        fmt.Fprint(w, "permission denied")
+        return
+    }
+    g := Greeting{
+        Author:  u.String(),
+        Content: r.FormValue("content"),
+        Date:    time.Now(),
+    }
 
-	key := datastore.NewIncompleteKey(c, "Greeting", guestbookKey(c))
-	_, err := datastore.Put(c, key, &g)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/", http.StatusFound)
+    key := datastore.NewIncompleteKey(c, "Greeting", guestbookKey(c))
+    _, err := datastore.Put(c, key, &g)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    http.Redirect(w, r, "/", http.StatusFound)
 }
 
 func edit(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	u := user.Current(c)
-	if u == nil {
-		fmt.Fprint(w, "permission denied")
-		return
-	}
-	id, err := strconv.Atoi(r.FormValue("id"))
-	key := datastore.NewKey(c, "Greeting", "", int64(id), guestbookKey(c))
-	greeting := new(Greeting)
-	err = datastore.Get(c, key, greeting)
-	greeting.Id = key.IntID()
-	if u.String() != greeting.Author {
-		fmt.Fprint(w, "permission denied")
-		return
-	}
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if err := editTemplate.Execute(w, greeting); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+    c := appengine.NewContext(r)
+    u := user.Current(c)
+    if u == nil {
+        fmt.Fprint(w, "permission denied")
+        return
+    }
+    id, err := strconv.Atoi(r.FormValue("id"))
+    key := datastore.NewKey(c, "Greeting", "", int64(id), guestbookKey(c))
+    greeting := new(Greeting)
+    err = datastore.Get(c, key, greeting)
+    greeting.Id = key.IntID()
+    if u.String() != greeting.Author {
+        fmt.Fprint(w, "permission denied")
+        return
+    }
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    if err := editTemplate.Execute(w, greeting); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 
 }
 
@@ -149,63 +149,63 @@ var editTemplate = template.Must(template.New("edit").Funcs(template.FuncMap{"eq
 
 const editTemplateHTML = `
 <html>
-	<body>
-		<form action="/update" method="post">
-			<div><textarea name="content" rows="3" cols="60">{{.Content}}</textarea></div>
-      		<div><input type="submit" value="update"><input type="hidden" name="id" value={{.Id}} /></div>
-		</form>
-	</body>
+    <body>
+        <form action="/update" method="post">
+            <div><textarea name="content" rows="3" cols="60">{{.Content}}</textarea></div>
+        <div><input type="submit" value="update"><input type="hidden" name="id" value={{.Id}} /></div>
+        </form>
+    </body>
 </html>
 `
 
 func update(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	u := user.Current(c)
-	if u == nil {
-		fmt.Fprint(w, "permission denied")
-		return
-	}
+    c := appengine.NewContext(r)
+    u := user.Current(c)
+    if u == nil {
+        fmt.Fprint(w, "permission denied")
+        return
+    }
 
-	id, err := strconv.Atoi(r.FormValue("id"))
-	key := datastore.NewKey(c, "Greeting", "", int64(id), guestbookKey(c))
-	greeting := new(Greeting)
-	greeting.Id = key.IntID()
-	err = datastore.Get(c, key, greeting)
+    id, err := strconv.Atoi(r.FormValue("id"))
+    key := datastore.NewKey(c, "Greeting", "", int64(id), guestbookKey(c))
+    greeting := new(Greeting)
+    greeting.Id = key.IntID()
+    err = datastore.Get(c, key, greeting)
 
-	if u.String() != greeting.Author {
-		fmt.Fprint(w, "permission denied")
-		return
-	}
+    if u.String() != greeting.Author {
+        fmt.Fprint(w, "permission denied")
+        return
+    }
 
-	greeting.Content = r.FormValue("content")
-	key, err = datastore.Put(c, key, greeting)
+    greeting.Content = r.FormValue("content")
+    key, err = datastore.Put(c, key, greeting)
 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 
-	w.Header().Set("Location", "/")
-	w.WriteHeader(http.StatusFound)
+    w.Header().Set("Location", "/")
+    w.WriteHeader(http.StatusFound)
 }
 
 func destroy(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	u := user.Current(c)
+    c := appengine.NewContext(r)
+    u := user.Current(c)
 
-	if u == nil {
-		fmt.Fprint(w, "permission denied")
-	}
+    if u == nil {
+        fmt.Fprint(w, "permission denied")
+    }
 
-	id, err := strconv.Atoi(r.FormValue("id"))
-	key := datastore.NewKey(c, "Greeting", "", int64(id), guestbookKey(c))
-	err = datastore.Delete(c, key)
+    id, err := strconv.Atoi(r.FormValue("id"))
+    key := datastore.NewKey(c, "Greeting", "", int64(id), guestbookKey(c))
+    err = datastore.Delete(c, key)
 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 
-	w.Header().Set("Location", "/")
-	w.WriteHeader(http.StatusFound)
+    w.Header().Set("Location", "/")
+    w.WriteHeader(http.StatusFound)
 }
